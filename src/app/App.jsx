49b5518a192a65fac97848c6688835da08647baf
matinhajizadeh-dev <matinhajizadeh.dev@gmail.com>
2026-06-26 19:26:02 +0330

    chore: create scalable project structure

---
+++ src/app/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+function App() {
+  return (
+    <>
+    <div className="flex min-h-screen items-center justify-center bg-slate-950">
+      <h1 className="text-5xl font-bold text-white">
+        RestoOS 🚀
+      </h1>
+    </div>
+    </>
+  )
+}
+
+export default App
